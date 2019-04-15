607dc42bef9adfd1cf6a70d4785913939bbfd567
Teo <teodor_paius@yahoo.com>
2019-04-15 15:44:57 +0300

    fixed

--- lab7/p1.m
+++ lab7/p1.m
@@ -1,16 +1,15 @@
 f = @(x) 2/(1+x^2);
-samples = 0:0.1:1;
+samples = 0:0.001:1;
 y = arrayfun(f, samples);
 
 
 a = 1;
 b = 2;
 n = size(samples,2);
-syms k;
 
 
 
-approx = (b-a)/2*n * (f(a)+f(b) + sum(arrayfun(f, samples(1:end-1))));
+approx = (b-a)/(2*n) * (f(a)+f(b) + 2*sum(arrayfun(f, samples(2:end-1))));
 
 fprintf("integral approx:  %f\n", approx);
 
@@ -19,3 +18,10 @@ hold on
 
 pgon = polyshape([0 0 1 1],[0 f(0) f(1) 0]);
 plot(pgon);
+
+
+%simpson
+
+temp = (samples(2:end) - samples(1:end-1))/2;
+
+approx_s = (b-a)/(6*n) * (f(a)+f(b) + 4*sum(arrayfun(f, temp))+ 2*sum(arrayfun(f, samples(2:end-1))))
