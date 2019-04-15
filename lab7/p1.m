b0107e2b8064a50355c40051616cd0c321ba3754
Teo <teodor_paius@yahoo.com>
2019-04-15 16:15:25 +0300

    now ex1 works

--- lab7/p1.m
+++ lab7/p1.m
@@ -1,11 +1,12 @@
-f = @(x) 2/(1+x^2);
-samples = 0:0.001:1;
+f = @(x) 2./(1+x.^2);
+samples = 0:0.01:1;
 y = arrayfun(f, samples);
 
+integral(f, 0,1)
 
-a = 1;
-b = 2;
-n = size(samples,2);
+a = 0;
+b = 1;
+n = size(samples,2)-1;
 
 
 
@@ -16,12 +17,15 @@ fprintf("integral approx:  %f\n", approx);
 plot(samples, y);
 hold on
 
+
 pgon = polyshape([0 0 1 1],[0 f(0) f(1) 0]);
 plot(pgon);
 
 
 %simpson
 
-temp = (samples(2:end) - samples(1:end-1))/2;
+temp = (samples(2:end) + samples(1:end-1))/2;
 
-approx_s = (b-a)/(6*n) * (f(a)+f(b) + 4*sum(arrayfun(f, temp))+ 2*sum(arrayfun(f, samples(2:end-1))))
+approx_s = (b-a)/(6*n) * (f(a)+f(b) + 4*sum(arrayfun(f, temp))+ 2*sum(arrayfun(f, samples(2:end-1))));
+
+fprintf("simpson approx:  %f\n", approx_s);
