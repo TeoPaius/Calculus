ae44e8b8a0d5f49cf75b2c40fd3b540f3629dd27
Teo <teodor_paius@yahoo.com>
2019-05-24 17:17:07 +0300

    lab10

--- lab9/p4.m
+++ lab9/p4.m
@@ -1,3 +1,14 @@
+fprintf("a\n");
+
+for n = 10:15
+    k = 1:n;
+    tk = 1 ./ k;
+    A = vander(tk);
+    cnd = cond(A);
+    fprintf("n = %d, cond(n) = %f\n", n, cnd);
+end
+
+fprintf("b\n");
 for n = 10:15
     k = 1:n;
     tk = -1 + (2/n) .* k;
